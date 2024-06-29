0ad291af70b62707f93559fc4106def1d43b678d
farg-eh <fargezbz@gmail.com>
2024-06-29 17:11:59 +0300

    an approach to fixing the menu game recursion bug -introduced a stack based state managment approach -cleaned up main.py it has less code now -moved all the menu classes into one class and minimised the code using inheritance -evey menu is a seperate state now meaning opening any menu will pause everything else

--- main.py
+++ main.py
@@ -1,26 +1,33 @@
 from src.settings import *
 from src.support import *
 from src.level import Level
-from src.main_menu import main_menu
-
+from src.menus import MainMenu, PauseMenu, SettingsMenu
 
 class Game:
-    def __init__(self):
+    def __init__(self, game_state):
+        self.game_state = game_state
+
+        # basics
+        pygame.init()
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.clock = pygame.time.Clock()
+        pygame.display.set_caption('PyDew')
+
+        # loading assets
         self.character_frames = None
         self.level_frames = None
         self.tmx_maps = None
         self.overlay_frames = None
         self.font = None
-        self.settings_menu = False
         self.sounds = None
-        pygame.init()
-        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-        pygame.display.set_caption('PyDew')
-        self.clock = pygame.time.Clock()
-        self.running = True
         self.load_assets()
-        self.running = True
-        self.level = Level(self.tmx_maps, self.character_frames, self.level_frames, self.overlay_frames, self.font, self.sounds)
+
+        # the level
+        self.level = Level(self.tmx_maps, self.character_frames, self.level_frames, self.overlay_frames, self.font, self.sounds, game_state)
+
+        # menus
+        self.pause_menu = PauseMenu(self.font, self.game_state)
+        self.settings_menu = SettingsMenu(self.font, self.game_state, sounds=self.sounds)
 
     def load_assets(self):
         self.tmx_maps = tmx_importer('data/maps')
@@ -43,75 +50,51 @@ class Game:
         self.font = import_font(30, 'font/LycheeSoda.ttf')
 
     def run(self):
-        while self.running:
+        while self.game_state[-1] == 'game':
             dt = self.clock.tick() / 1000
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
-            keys = pygame.key.get_just_pressed()
+                    while self.game_state[-1] != 'quit':
+                        self.game_state.pop()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        self.pause_menu.bg = self.screen.copy()
+                        self.settings_menu.bg = self.screen.copy()
+                        self.game_state.append('pause_menu')
+
+
             self.screen.fill('gray')
             self.level.update(dt)
-            if self.level.entities["Player"].paused:
-                pause_menu = self.level.entities["Player"].pause_menu
-                self.settings_menu = False
-                if pause_menu.pressed_play:
-                    self.level.entities["Player"].paused = not self.level.entities["Player"].paused
-                    pause_menu.pressed_play = False
-                elif pause_menu.pressed_quit:
-                    pause_menu.pressed_quit = False
-                    self.running = False
-                    game = MainMenu()
-                    game.run()
-                elif pause_menu.pressed_settings:
-                    self.settings_menu = self.level.entities["Player"].settings_menu
-                if self.settings_menu and self.settings_menu.go_back:
-                    self.settings_menu.go_back = False
-                    self.settings_menu = False
-                    pause_menu.pressed_settings = False
-                if self.settings_menu == False:
-                    pause_menu.update()
-                if self.settings_menu:
-                    self.settings_menu.update()
-            if self.settings_menu != False:
-                if keys[pygame.K_ESCAPE]:
-                    self.settings_menu = False
-                    pause_menu.pressed_settings = False
 
             pygame.display.update()
 
-class MainMenu:
+
+class Manager:
     def __init__(self):
-        self.menu = True
-        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-        pygame.init()
-        self.font = import_font(30, 'font/LycheeSoda.ttf')
-        pygame.display.set_caption('PyDew')
-        self.clock = pygame.time.Clock()
-        self.sounds = sound_importer('audio', default_volume=0.25)
-        self.main_menu = main_menu(self.font, self.sounds["music"])
-        self.background = pygame.image.load("images/menu_background/bg.png")
-        self.background = pygame.transform.scale(self.background, (SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.states = ['quit', 'main_menu']
+        self.game = Game(self.states)
+        menu_bg = pygame.image.load(join('images', 'menu_background', 'bg.png')).convert()
+        self.main_menu = MainMenu(self.game.font, self.states, bg=menu_bg)
+
     def run(self):
-        while self.menu:
-            dt = self.clock.tick() / 1000
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
+        while True:
+            match(self.states[-1]):
+                case 'main_menu':
+                    self.main_menu.run()
+                case 'game':
+                    self.game.run()
+                case 'pause_menu':
+                    self.game.pause_menu.run()
+                case 'settings_menu':
+                    self.game.settings_menu.run()
+                case 'shop_menu':
+                    self.game.level.shop_menu.run()
+                case 'quit':
                     pygame.quit()
                     sys.exit()
-            if self.main_menu.pressed_play:
-                self.sounds["music"].stop()
-                self.menu = False
-                game = Game()
-                game.run()
-            elif self.main_menu.pressed_quit:
-                self.menu = False
-                pygame.quit()
-                sys.exit()
-            self.screen.blit(self.background, (0, 0))
-            self.main_menu.update()
-            pygame.display.update()
+                case _:
+                    print("\033[91munexpected state\033[0m")
+                    self.states = ['quit']
 
 if __name__ == '__main__':
-    game = MainMenu()
-    game.run()
+    Manager().run()
