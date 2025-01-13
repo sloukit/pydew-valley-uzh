a7a43a438f70184aa9f536da6a76a72817d25dab
sloukit <120415680+sloukit@users.noreply.github.com>
2025-01-13 07:25:28 +0100

    Set game language to German

--- run_game.sh
+++ run_game.sh
@@ -1,6 +1,6 @@
 # set game language (en or de)
-if [ "$1" ]; then
-    export GAME_LANGUAGE=$1
+if [ "de" ]; then
+    export GAME_LANGUAGE=de
 fi
 # redirecting stderr to dev/null to get rid of "+[IMKClient subclass]: chose IMKClient_Modern" messages
 python main.py
